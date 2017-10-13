a218b2f2437cbcaffb164eb4125a655a6396e35a
liulinhui <1249055292@qq.com>
2017-10-13 20:09:06 +0800

    ignore

--- src/main/java/com/activiti/common/kafka/Listener.java
+++ src/main/java/com/activiti/common/kafka/Listener.java
@@ -12,7 +12,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 
-import javax.mail.MessagingException;
 import java.util.Optional;
 
 /**
