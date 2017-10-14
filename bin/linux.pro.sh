691f303c56a7acd5b76169debe9b385de869a895
liulinhui <1249055292@qq.com>
2017-10-14 23:38:03 +0800

    ignore

--- bin/linux.pro.sh
+++ bin/linux.pro.sh
@@ -5,4 +5,4 @@ git pull
 cd bin/
 ./package.sh
 cd ../target/
-java -Xms1024m -Xmx1024m -jar work-flow.jar --spring.profiles.active=pro --server.port=8080
+java -Xms2048m -Xmx2048m -jar work-flow.jar --spring.profiles.active=pro --server.port=8080
