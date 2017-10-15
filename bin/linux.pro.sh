17edae19f0c303ec80be5d11c3052c4bc3bd7eae
liulinhui <1249055292@qq.com>
2017-10-16 01:28:38 +0800

    bug

--- bin/linux.pro.sh
+++ bin/linux.pro.sh
@@ -5,4 +5,4 @@ git pull
 cd bin/
 ./package.sh
 cd ../target/
-java -Xms2048m -Xmx2048m -jar work-flow.jar --spring.profiles.active=pro --server.port=8080
+java -Xms2048m -Xmx2048m -Duser.timezone=GMT+8 -jar work-flow.jar --spring.profiles.active=pro --server.port=8080
